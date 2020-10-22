23b81e3fc04044bc4b114e982f677c6224e1bdae
Jordan C <nykezgaming@live.com>
2020-10-22 16:20:26 -0400

    add prototype game class

---
+++ server/classes/game.js
@@ -0,0 +1,18 @@
+const Player = require("./player");
+
+function Game(id, player_host, hostId, name)
+{
+    this.id = id;
+    this.host = player_host;
+    this.hostId = hostId;
+    this.name = name;
+    this.players = [];
+};
+
+Game.prototype.addPlayer = function(player)
+{
+  this.players.push(player);
+  console.log("added a player to game lobby" + this.hostId + "[" + player + "]");
+};
+
+module.exports = Game;
