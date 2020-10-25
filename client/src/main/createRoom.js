1c7b9daf3981c76e48210409234398869eb7a766
Jordan C <nykezgaming@live.com>
2020-10-24 23:57:08 -0400

    add createRoom page

--- client/src/main/createRoom.js
+++ client/src/main/createRoom.js
@@ -1,16 +1,54 @@
 import React, { Component } from "react";
-import { Button, Table, Container } from "reactstrap";
+import { socket } from '../global/header'
 
 
 class CreateRoom extends Component{
     constructor(){
         super();
+        this.state = {
+            username: "",
+            roomname: "a fun game!",
+        }
+        
+        // setup binders
+        this.setUsername = this.setUsername.bind(this);
+        this.setRoomName = this.setRoomName.bind(this);
+    }
+
+    // Set the username state
+    setUsername(event)
+    {
+        this.setState({username: event.target.value})
+    }
+
+    // Set the room name state
+    setRoomName(event)
+    {
+        this.setState({roomname: event.target.value})
+    }
+
+    // Submit changes -> create a room
+    onSubmit()
+    {
+        console.log(this.state);
+        console.log(socket);
     }
 
     render(){
         return(
-            <div class="container">
-                
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-6">
+                        <form>
+                            <label>What's your nickname?</label>
+                            <input type="text" value={this.state.username} onChange={this.setUsername} className="form-control" placeholder="Johnny Appleseed"/>
+                            <label>Name of your room?</label>
+                            <input type="text" value={this.state.roomname} onChange={this.setRoomName} className="form-control" placeholder={this.state.roomname}/>
+                        </form>
+                        <button type="button" className="btn btn-primary mt-1" onClick={() => this.onSubmit()}>Create Room</button>
+                        <button type="button" className="btn btn-secondary ml-1 mt-1">Help Guide</button>
+                    </div>
+                </div>
             </div>
         );
     };
---
+++ client/src/main/joinRoom.js
@@ -0,0 +1,8 @@
+
+import React, { Component } from "react";
+
+class JoinRoom extends Component{
+    constructor() {
+        super();
+    }
+}
