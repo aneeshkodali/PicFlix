016c60fd59db4d72eefb18bdcc0a870972912eb6
Jordan C <nykezgaming@live.com>
2020-10-26 00:08:52 -0400

    playerlist component

--- client/src/components/playerList.js
+++ client/src/components/playerList.js
@@ -1,8 +1,18 @@
 import React, { Component } from "react";
 
-class PlayerList extends Component{
+const PlayerList = ({playerList}) => {
 
-    
+    var players = ['Player 1','Player 2', 'Player 3'];
+
+    const listPlayers = players.map((number) =>    
+        <li>{number} <span class="badge badge-pill badge-primary"> 0 </span></li>  
+    );
+
+    return(
+        <div>
+            {listPlayers}
+        </div>
+    )
 };
 
 export default PlayerList;
