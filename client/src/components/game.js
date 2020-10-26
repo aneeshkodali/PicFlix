6eedd057e3de17943502844c7ff66619a960e5c8
Jordan C <nykezgaming@live.com>
2020-10-26 00:14:20 -0400

    some main lobby work

--- client/src/components/game.js
+++ client/src/components/game.js
@@ -5,6 +5,8 @@ import { socket } from './Header';
 
 import SearchBar from './Image/SearchBar';
 import ImageList from './Image/ImageList';
+import PlayerList from './playerList';
+
 import { Link } from 'react-router-dom';
 
 const Game = () => {
@@ -24,23 +26,23 @@ const Game = () => {
         setImagesData(response.data.results);
     }
 
-    // if (!socket.roomname)
-    // {
-    //     return (
-    //         <div className="container text-center">
-    //             <h1>No game found!</h1>
-    //             <h4>
-    //                 Consider
-    //                 <Link to='/create-room'> creating </Link>
-    //                 or
-    //                 <Link to='/join-room'> joining </Link>
-    //                 one!
-    //             </h4>
-    //         </div>
-    //     )
-    // }
-    // else if (socket.roomname.length > 0)
-    // {
+    if (!socket.roomname)
+    {
+        return (
+            <div className="container text-center">
+                <h1>No game found!</h1>
+                <h4>
+                    Consider
+                    <Link to='/create-room'> creating </Link>
+                    or
+                    <Link to='/join-room'> joining </Link>
+                    one!
+                </h4>
+            </div>
+        )
+    }
+    else if (socket.roomname.length > 0)
+    {
         return (
             <div className="container">
                 {/* Image Screen */}
@@ -51,20 +53,27 @@ const Game = () => {
                             <div className="card-header">
                                 <h5>Current Image</h5>
                             </div>
+                            <img src="https://picsum.photos/200" className="rounded mx-auto d-block mt-1 mb-1" alt="..."></img>
                         </div>
                     </div>
 
                     {/* Player List */}
-                    <div className="col-3" style={{height: '20%'}}>
+                    <div className="col-3">
                         <div className="card">
                             <div className="card-header">
                                 <h5>Player List</h5>
-                                
+                                <PlayerList/>
                             </div>
+                        </div>
+                        <div className="row mt-1 mx-auto">
+                            <button className="btn btn-primary mr-1">Leave Game</button>
+                            <button className="btn btn-secondary mr-1">Read Guide</button>
                         </div>
                     </div>
 
                 </div>
+                <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} searchFunc={search} />
+                <ImageList imagesData={imagesData} />
 
                 {/*  Chat Log/Guess Log */}
                 <div className="row content-justify-center">
@@ -77,11 +86,10 @@ const Game = () => {
                     </div>
 
                 </div>
-                {/* <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} searchFunc={search} />
-                <ImageList imagesData={imagesData} /> */}
+
             </div>
         )
-    // }
+    }
     
 
 
